053318e49ff525f21e1b97eb73454a75a1ce488c
dfreire <dario.freire@gmail.com>
2015-07-15 23:33:31 +0100

    update

--- auth/auth.go
+++ auth/auth.go
@@ -27,21 +27,24 @@ func (self impl) signUp(email string, password string) (signUpToken string, err 
 		return "", err
 	}
 
-	return "", nil
-}
-
-func (self impl) assertCanCreateUser(email string) error {
-	hasUserWithEmail, err := self.store.hasUserWithEmail(email)
+	signUpToken, err = createSignUpToken(email)
 	if err != nil {
-		return err
-	} else if hasUserWithEmail {
-		return errors.New(ErrEmailAlreadyUsed)
+		return "", err
 	}
 
-	return nil
+	if err = self.store.putPendingUser(email, password); err != nil {
+		return "", err
+	}
+
+	return signUpToken, nil
 }
 
 func (self impl) confirmSignUp(signUpToken string) error {
+	email, err := parseSignUpToken(signUpToken)
+	if err != nil {
+		return err
+	}
+
 	if err := self.assertCanCreateUser(email); err != nil {
 		return err
 	}
@@ -50,6 +53,12 @@ func (self impl) confirmSignUp(signUpToken string) error {
 	if err != nil {
 		return err
 	}
+
+	if err = self.store.createUser(User(pendingUser)); err != nil {
+		return err
+	}
+
+	err = self.store.delPendingUser(email)
 
 	return nil
 }
@@ -72,4 +81,23 @@ func (self impl) changeEmail(sessionToken string, password string, newEmail stri
 
 func (self impl) changePassword(sessionToken string, oldPassword string, newPassword string) error {
 	return nil
+}
+
+func (self impl) assertCanCreateUser(email string) error {
+	hasUserWithEmail, err := self.store.hasUserWithEmail(email)
+	if err != nil {
+		return err
+	} else if hasUserWithEmail {
+		return errors.New(ErrEmailAlreadyUsed)
+	}
+
+	return nil
+}
+
+func createSignUpToken(email string) (signUpToken string, err error) {
+	return "", nil
+}
+
+func parseSignUpToken(signUpToken string) (email string, err error) {
+	return "", nil
 }
--- auth/store.go
+++ auth/store.go
@@ -3,6 +3,8 @@ package auth
 type store interface {
 	createUser(user User) error
 	hasUserWithEmail(email string) (bool, error)
-	putPendingUser(email string, password string) error
+
 	getPendingUser(email string) (PendingUser, error)
+	putPendingUser(email string, password string) error
+	delPendingUser(email string) error
 }
