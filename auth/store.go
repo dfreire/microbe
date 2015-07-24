0d9d3368cc935f9e5f367af557dd3317c04cd281
dfreire <dario.freire@gmail.com>
2015-07-24 23:12:09 +0100

    update

--- auth/store.go
+++ auth/store.go
@@ -1,40 +1,8 @@
 package auth
 
-import "github.com/satori/go.uuid"
-
 type store interface {
 	getUser(domain, email string) (User, error)
 
 	createUser(user User) error
 	updateUser(user User) error
-}
-
-type implStoreMem struct {
-	usersById map[interface{}]User
-}
-
-func NewStoreMem() store {
-	return &implStoreMem{}
-}
-
-func (self implStoreMem) getUser(domain, email string) (User, error) {
-	var user User
-	for _, existentUser := range self.usersById {
-		if existentUser.Domain == domain && existentUser.Email == email {
-			user = existentUser
-			break
-		}
-	}
-	return user, nil
-}
-
-func (self implStoreMem) createUser(user User) error {
-	user.Id = uuid.NewV1()
-	self.usersById[user.Id] = user
-	return nil
-}
-
-func (self implStoreMem) updateUser(user User) error {
-	self.usersById[user.Id] = user
-	return nil
 }
--- auth/storemem.go
+++ auth/storemem.go
@@ -1,1 +1,33 @@
 package auth
+
+import "github.com/satori/go.uuid"
+
+type implStoreMem struct {
+	usersById map[interface{}]User
+}
+
+func NewStoreMem() store {
+	return &implStoreMem{}
+}
+
+func (self implStoreMem) getUser(domain, email string) (User, error) {
+	var user User
+	for _, existentUser := range self.usersById {
+		if existentUser.Domain == domain && existentUser.Email == email {
+			user = existentUser
+			break
+		}
+	}
+	return user, nil
+}
+
+func (self implStoreMem) createUser(user User) error {
+	user.Id = uuid.NewV1()
+	self.usersById[user.Id] = user
+	return nil
+}
+
+func (self implStoreMem) updateUser(user User) error {
+	self.usersById[user.Id] = user
+	return nil
+}
