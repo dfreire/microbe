fd2cfe08e74e5e85ced44e5e01a25a1d65b324b6
dfreire <dario.freire@gmail.com>
2015-07-24 22:36:01 +0100

    update

--- auth/store.go
+++ auth/store.go
@@ -1,5 +1,11 @@
 package auth
 
+import (
+	"errors"
+
+	"github.com/satori/go.uuid"
+)
+
 type store interface {
 	getUser(domain, email string) (User, error)
 
@@ -8,7 +14,7 @@ type store interface {
 }
 
 type implStoreMem struct {
-	usersById map[string]User
+	usersById map[interface{}]User
 }
 
 func NewStoreMem() store {
@@ -17,13 +23,25 @@ func NewStoreMem() store {
 
 func (self implStoreMem) getUser(domain, email string) (User, error) {
 	var user User
+	for _, existentUser := range self.usersById {
+		if existentUser.Domain == domain && existentUser.Email == email {
+			user = existentUser
+			break
+		}
+	}
+	if user.Id == "" {
+		return user, errors.New(ErrEntityNotFound)
+	}
 	return user, nil
 }
 
 func (self implStoreMem) createUser(user User) error {
+	user.Id = uuid.NewV1()
+	self.usersById[user.Id] = user
 	return nil
 }
 
 func (self implStoreMem) updateUser(user User) error {
+	self.usersById[user.Id] = user
 	return nil
 }
