34e03d58de9e95d4ac895aaf63e534adb50abb48
dfreire <dario.freire@gmail.com>
2015-07-27 21:52:29 +0100

    update

--- checkout/model.go
+++ checkout/model.go
@@ -9,9 +9,10 @@ type Order struct {
 	InvoiceAddressId  interface{}
 	DeliveryAddressId interface{}
 	DeliveryMethod    DeliveryMethod
-	Lines             []OrderLine
 	PaymentMethod     PaymentMethod
-	Status            OrderStatus
+	Lines             []OrderLine
+	OrderStatus       OrderStatus
+	PaymentStatus     PaymentStatus
 }
 
 type OrderLine struct {
@@ -47,24 +48,31 @@ type Address struct {
 type PaymentMethod int
 
 const (
-	CreditCard PaymentMethod = iota
-	BankTransfer
-	ATMTrasnfer
+	PAYMENT_METHOD_CREDIT_CARD PaymentMethod = iota
+	PAYMENT_METHOD_BANK_TRANSFER
+	PAYMENT_METHOD_ATM
 )
 
 type DeliveryMethod int
 
 const (
-	CTTEconomic DeliveryMethod = iota
-	CTTExpress
+	DELIVERY_METHOD_CTT_ECONOMIC DeliveryMethod = iota
+	DELIVERY_METHOD_CTT_EXPRESS
 )
 
 type OrderStatus int
 
 const (
-	PaymentPending OrderStatus = iota
-	PaymentUnauthorized
-	Confirmed
-	Sent
-	Delivered
+	ORDER_STATUS_PENDING OrderStatus = iota
+	ORDER_STATUS_CONFIRMED
+	ORDER_STATUS_SENT
+	ORDER_STATUS_DELIVERED
+)
+
+type PaymentStatus int
+
+const (
+	PAYMENT_STATUS_PENDING PaymentStatus = iota
+	PAYMENT_STATUS_UNAUTHORIZED
+	PAYMENT_STATUS_AUTHORIZED
 )
